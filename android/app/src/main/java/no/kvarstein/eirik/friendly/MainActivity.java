dc5ad4e9a2be7ff5dcb6883e0e829832a9b0eab2
everystone <eirik.kvarstein@gmail.com>
2015-04-14 19:27:20 +0200

    Timer using Handler, Cached location from GPS & HTTP Async

--- android/app/src/main/java/no/kvarstein/eirik/friendly/MainActivity.java
+++ android/app/src/main/java/no/kvarstein/eirik/friendly/MainActivity.java
@@ -3,8 +3,10 @@ package no.kvarstein.eirik.friendly;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -18,28 +20,87 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+    utils Utils = null;
+    final long interval_delay = 10; //Delay between requests. In seconds, read from config.
+    final boolean app_enabled = true;
+    TextView debug;
+    private String username;
+    //runs without a timer by reposting this handler at the end of the runnable
+
+
+    private void sendLocation(Location pos){
+        //Create the URL
+        String url = "http://eirik.pw:3000/loc/" + username + "/" + pos.getLatitude() + "/" + pos.getLongitude();
+
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute(url);
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return Utils.GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            //Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            //etResponse.setText(result);
+            debug.setText(result);
+        }
+    }
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+
+            //Get GPS location
+            //Test location
+             Location test = Utils.getLastPos();
+            // String nfo = test.getLatitude() + ", "+test.getLongitude() + " alt: "+test.getAltitude();
+             String nfo = String.format("%.5f, %.5f, %.5f", test.getLatitude(), test.getLongitude(), test.getAltitude());
+             toast(nfo);
+
+            sendLocation(test);
+
+           // timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+
+            if(app_enabled){
+            timerHandler.postDelayed(this, interval_delay * 1000);
+            }
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        final EditText usrText = (EditText)findViewById(R.id.usrNameText);
-        final Button newUserBtn = (Button)findViewById(R.id.newusrbtn);
+        final EditText usrText = (EditText) findViewById(R.id.usrNameText);
+        final Button newUserBtn = (Button) findViewById(R.id.newusrbtn);
         final Context mContext = getApplicationContext();
+        debug= (TextView)findViewById(R.id.debug);
+
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        final utils Utils = new utils(locationManager);
+        Utils = new utils(locationManager);
+
+
         //Hook up our new User button to a method.
         newUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String usrname = usrText.getText().toString();
 
-                Toast.makeText(getApplicationContext(), "Username: "+usrname,
+                Toast.makeText(getApplicationContext(), "Username: " + usrname,
                         Toast.LENGTH_SHORT).show();
-               // Log.e("ReadWrite", "Username input: " + usrname);
-            Utils.writeUserName(usrname, mContext);
-                TextView usr = (TextView)findViewById(R.id.username);
+                // Log.e("ReadWrite", "Username input: " + usrname);
+                Utils.writeUserName(usrname, mContext);
+                TextView usr = (TextView) findViewById(R.id.username);
                 usr.setText(Utils.readUserName(mContext));
                 usrText.setVisibility(View.INVISIBLE);
                 newUserBtn.setVisibility(View.INVISIBLE);
@@ -47,28 +108,25 @@ public class MainActivity extends ActionBarActivity {
         });
 
 
-
         //Check if userfile exist
-        if(Utils.readUserName(this) == null){
+        if (Utils.readUserName(this) == null) {
             //Userfile does not exist, prompt for username with popup
             //Make controlls visible
 
             usrText.setVisibility(View.VISIBLE);
             newUserBtn.setVisibility(View.VISIBLE);
 
-        }else {
+        } else {
             //Read username from file, display info
-            TextView usr = (TextView)findViewById(R.id.username);
-            usr.setText(Utils.readUserName(mContext));
+            TextView usr = (TextView) findViewById(R.id.username);
+            username = Utils.readUserName(mContext);
+            usr.setText(username);
+            debug.setVisibility(View.VISIBLE);
         }
 
 
-        //Test location
-        Location test = Utils.getLastPos();
-       // String nfo = test.getLatitude() + ", "+test.getLongitude() + " alt: "+test.getAltitude();
-        String nfo = String.format("%.5f, %.5f, %.5f", test.getLatitude(), test.getLongitude(), test.getAltitude());
-        toast(nfo);
-
+        //Start main thread, sends GPS location every X minutes
+        timerHandler.postDelayed(timerRunnable, 0);
     }
 
     private void toast(String msg){
--- android/app/src/main/java/no/kvarstein/eirik/friendly/utils.java
+++ android/app/src/main/java/no/kvarstein/eirik/friendly/utils.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.*;
 
@@ -20,12 +26,20 @@ public class utils{
 
     }
 
+
+    public void sendLocation(Location pos, String username){
+
+
+    }
+
     public Location getLastPos(){
         String locationProvider = LocationManager.GPS_PROVIDER; //NETWORK_PROVIDER
         // Or use LocationManager.GPS_PROVIDER
 
         Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
         if(lastKnownLocation == null){
+            //Testing..
+            lastKnownLocation = new Location(locationProvider);
             lastKnownLocation.setLatitude(0);
             lastKnownLocation.setAltitude(100);
             lastKnownLocation.setLongitude(0);
@@ -80,6 +94,48 @@ public class utils{
         }
 
         return ret;
+    }
+
+
+    /* Send HTTP Get request and catch response */
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 
 }
